fdf80f460ba31175ec999729911034e0419f71fd
Andrewmatilde <davis6813585853062@outlook.com>
2024-11-14 18:30:40 +0000

    Add more log to chaosdaemon/httpchaos
    Signed-off-by: Andrewmatilde <davis6813585853062@outlook.com>

--- pkg/chaosdaemon/httpchaos_server.go
+++ pkg/chaosdaemon/httpchaos_server.go
@@ -21,6 +21,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
 	"os"
@@ -72,17 +73,21 @@ func (t *stdioTransport) RoundTrip(req *http.Request) (resp *http.Response, err 
 
 func (s *DaemonServer) ApplyHttpChaos(ctx context.Context, in *pb.ApplyHttpChaosRequest) (*pb.ApplyHttpChaosResponse, error) {
 	log := s.getLoggerFromContext(ctx)
-	log.Info("applying http chaos")
+	log.Info("applying http chaos", "in.InstanceUid", in.InstanceUid)
 
 	if in.InstanceUid == "" {
+		log.Info("instance uid is empty, try to get it from background process manager")
 		if uid, ok := s.backgroundProcessManager.GetUID(bpm.ProcessPair{Pid: int(in.Instance), CreateTime: in.StartTime}); ok {
+			log.Info("get instance uid from background process manager", "uid", uid)
 			in.InstanceUid = uid
 		}
 	}
 
 	if _, ok := s.backgroundProcessManager.GetPipes(in.InstanceUid); !ok {
+		log.Info("instance not found, create a new one")
 		if in.InstanceUid != "" {
 			// chaos daemon may restart, create another tproxy instance
+
 			if err := s.backgroundProcessManager.KillBackgroundProcess(ctx, in.InstanceUid); err != nil {
 				// ignore this error
 				log.Error(err, "kill background process", "uid", in.InstanceUid)
@@ -110,6 +115,7 @@ func (s *DaemonServer) applyHttpChaos(ctx context.Context, in *pb.ApplyHttpChaos
 
 	pipes, ok := s.backgroundProcessManager.GetPipes(in.InstanceUid)
 	if !ok {
+		log.Error(errors.Errorf("fail to get process(%s)", in.InstanceUid), "in.InstanceUid", in.InstanceUid)
 		return nil, errors.Errorf("fail to get process(%s)", in.InstanceUid)
 	}
 
@@ -122,9 +128,9 @@ func (s *DaemonServer) applyHttpChaos(ctx context.Context, in *pb.ApplyHttpChaos
 	var rules []tproxyconfig.PodHttpChaosBaseRule
 	err := json.Unmarshal([]byte(in.Rules), &rules)
 	if err != nil {
+		log.Error(err, "unmarshal rules", "rules", in.Rules)
 		return nil, errors.Wrap(err, "unmarshal rules")
 	}
-
 	log.Info("the length of actions", "length", len(rules))
 
 	httpChaosSpec := tproxyconfig.Config{
@@ -142,6 +148,7 @@ func (s *DaemonServer) applyHttpChaos(ctx context.Context, in *pb.ApplyHttpChaos
 
 	config, err := json.Marshal(&httpChaosSpec)
 	if err != nil {
+		log.Error(err, "marshal config", "config", httpChaosSpec)
 		return nil, err
 	}
 
@@ -149,11 +156,13 @@ func (s *DaemonServer) applyHttpChaos(ctx context.Context, in *pb.ApplyHttpChaos
 
 	req, err := http.NewRequest(http.MethodPut, "/", bytes.NewReader(config))
 	if err != nil {
+		log.Error(err, "create http request")
 		return nil, errors.Wrap(err, "create http request")
 	}
 
 	resp, err := transport.RoundTrip(req)
 	if err != nil {
+		log.Error(err, "send http request")
 		return nil, errors.Wrap(err, "send http request")
 	}
 
@@ -161,9 +170,11 @@ func (s *DaemonServer) applyHttpChaos(ctx context.Context, in *pb.ApplyHttpChaos
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
+		log.Error(err, "read response body")
 		return nil, errors.Wrap(err, "read response body")
 	}
 
+	log.Info("response", "status", resp.StatusCode, "body", string(body))
 	return &pb.ApplyHttpChaosResponse{
 		Instance:    int64(in.Instance),
 		InstanceUid: in.InstanceUid,
@@ -176,13 +187,16 @@ func (s *DaemonServer) applyHttpChaos(ctx context.Context, in *pb.ApplyHttpChaos
 func (s *DaemonServer) createHttpChaos(ctx context.Context, in *pb.ApplyHttpChaosRequest) error {
 	pid, err := s.crClient.GetPidFromContainerID(ctx, in.ContainerId)
 	if err != nil {
+		log.Error(err, "get PID of container", "containerId", in.ContainerId)
 		return errors.Wrapf(err, "get PID of container(%s)", in.ContainerId)
 	}
+	log.Info("get PID of container", "containerId", in.ContainerId, "pid", pid)
 	processBuilder := bpm.DefaultProcessBuilder(tproxyBin, "-i", "-vv").
 		EnableLocalMnt().
 		SetIdentifier(fmt.Sprintf("tproxy-%s", in.ContainerId)).
 		SetEnv(pathEnv, os.Getenv(pathEnv))
 
+	log.Info("Check if enter network namespace", "enterNS", in.EnterNS)
 	if in.EnterNS {
 		processBuilder = processBuilder.SetNS(pid, bpm.PidNS).SetNS(pid, bpm.NetNS)
 	}
@@ -190,13 +204,16 @@ func (s *DaemonServer) createHttpChaos(ctx context.Context, in *pb.ApplyHttpChao
 	cmd := processBuilder.Build(ctx)
 	cmd.Stderr = os.Stderr
 
+	log.Info("execute command", "cmd", cmd.String())
 	proc, err := s.backgroundProcessManager.StartProcess(ctx, cmd)
 	if err != nil {
+		log.Error(err, "execute command", "cmd", cmd.String())
 		return errors.Wrapf(err, "execute command(%s)", cmd)
 	}
 
 	in.Instance = int64(proc.Pair.Pid)
 	in.StartTime = proc.Pair.CreateTime
 	in.InstanceUid = proc.Uid
+	log.Info("create base for http chaos", "in.InstanceUid", in.InstanceUid)
 	return nil
 }
